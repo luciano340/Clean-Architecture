051dee8bda4373348927701664fd713265dfde5e
Luciano Romao <lucianosouzaromao@gmail.com>
2023-08-24 16:23:46 -0300

    feat: presenter to xml customer

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -84,5 +84,25 @@ describe("Teste End to End para Customer", () => {
         expect(customer2.name).toBe("Taynara");
         expect(customer2.address.street).toBe("Rua Ouro Fino");
 
+        const listResponseXML = await request(app)
+        .get("/customer")
+        .set("Accept", "application/xml")
+        .send();
+
+        expect(listResponseXML.status).toBe(200);
+        expect(listResponseXML.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
+        expect(listResponseXML.text).toContain(`<customers>`);
+        expect(listResponseXML.text).toContain(`<customer>`);
+        expect(listResponseXML.text).toContain(`<name>Luciano</name>`);
+        expect(listResponseXML.text).toContain(`<address>`);
+        expect(listResponseXML.text).toContain(`<street>Servidão Pedro Manoel dos Santos</street>`);
+        expect(listResponseXML.text).toContain(`<city>Floripa</city>`);
+        expect(listResponseXML.text).toContain(`<number>122</number>`);
+        expect(listResponseXML.text).toContain(`<zip>88058-479</zip>`);
+        expect(listResponseXML.text).toContain(`</address>`);
+        expect(listResponseXML.text).toContain(`</customer>`);
+        expect(listResponseXML.text).toContain(`<name>Taynara</name>`);
+        expect(listResponseXML.text).toContain(`<street>Rua Ouro Fino</street>`);
+        expect(listResponseXML.text).toContain(`</customers>`);
     });
 })
--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import CustomerCreateUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
 import CustomerRepository from "../../customer/repository/sequilize/customer.repository";
+import CustomerPresenter from "../presenters/customer.presenter";
 
 export const customerRoute = express.Router();
 
@@ -30,11 +31,11 @@ customerRoute.post('/', async (req: Request, res: Response) => {
 customerRoute.get("/", async (req:Request, res: Response) => {
     const  customerRepository = new CustomerRepository();
     const useCase = new ListCustomerUseCase(customerRepository);
+    const output = await useCase.execute({});
 
-    try {
-        const output = await useCase.execute({});
-        res.send(output)
-    } catch (error){
-        res.status(500).send(error)
-    }
+    res.format({
+       json: async () => res.send(output),
+       xml: async () => res.send(CustomerPresenter.toXML(output))
+    });
+
 });
